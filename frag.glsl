386053a52ce4097cbaf5f31c1d5cf4d5f7f3e9fb
Bobby Roe <roebobby@bobbys-laptop.lan>
2023-02-19 16:40:50 -0800

    center it up

--- frag.glsl
+++ frag.glsl
@@ -8,7 +8,7 @@ uniform float time;
 void main () {
   vec2 st = gl_FragCoord.xy / resolution;
   st.x *= resolution.x / resolution.y;
-  float dist = distance(st, vec2(0.4, 0.5)) * 2.0;
+  float dist = distance(st, vec2(0.5, 0.5)) * 2.0;
   dist = step(0.5, dist);
   vec3 hue = 0.5 + 0.5 * cos(time + st.xyx + vec3(1.0, 2.0, 4.0));
   vec3 color = vec3(1.0 - dist) * hue;
